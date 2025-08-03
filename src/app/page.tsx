8871d8b0c8df9362f24fed42e896c3b4ceb3fb11
Gustavo Gantois <gugagantois@gmail.com>
2025-08-03 20:27:54 -0300

    feat:Create component Hero CTA section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { AboutHome } from "./c-about";
+import { AboutHome } from "./s-about";
+import { CTAHome } from "./s-home-cta";
 
 export default function Page() {
   return (
     <>
+    <CTAHome />
     <AboutHome />
     </>
   );
---
+++ src/app/s-home-cta.tsx
@@ -0,0 +1,7 @@
+export function CTAHome() {
+    return (
+        <section className="">
+            
+        </section>
+    )
+}
